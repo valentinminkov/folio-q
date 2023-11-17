ff5ca443e190c98e8e80dc41f996ff399b09dc14
valentinminkov <minkowalentin@proton.me>
2023-11-18 00:32:20 +0100

    basic vue quasar setup

---
+++ src/dummytData.ts
@@ -0,0 +1,47 @@
+import type {
+  Button,
+  HeroBlock,
+  Image,
+  InfoBlock,
+  Card,
+} from 'components/types/components';
+import type { VNode } from 'vue';
+
+export const buttonDummyData: Button = {
+  text: 'Click me!',
+  url: '/some-url',
+};
+
+export const heroBlockDummyData: HeroBlock = {
+  title: 'Hero block dummy title!',
+  button: buttonDummyData,
+  subtitle: 'We offer services.',
+  className: 'hero-class',
+  image: 'path/to/hero-image.jpg',
+};
+
+export const imageDummyData: Image = {
+  src: 'path/to/image.jpg',
+  alt: 'Some description',
+  className: 'image-class',
+};
+
+export const infoBlockDummyData: InfoBlock = {
+  title: 'Info Block Title',
+  text: 'This is some information about our product.',
+  iconUrl: 'path/to/icon.png',
+  direction: 'horizontal',
+  className: 'info-block-class',
+  image: imageDummyData,
+  secondColumnContent: {} as VNode,
+};
+
+export const infoCardDummyData: Card = {
+  title: 'Info Card Title',
+  text: 'This card provides some valuable information.',
+  image: imageDummyData,
+  onClick: () => {
+    console.log('Card clicked!');
+  },
+  className: 'info-card-class',
+};
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-layout view="hhh lpr fff">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="favicon.svg" />
+          </q-avatar>
+          <div>VM</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+});
+</script>
