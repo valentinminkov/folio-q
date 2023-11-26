9bc36d0871aa668e3839c2fce78dba9586380a0b
valentinminkov <minkowalentin@proton.me>
2023-11-26 21:06:39 +0100

    basic components types

--- src/components/types/components.ts
+++ src/components/types/components.ts
@@ -1,40 +1,40 @@
-import type { VNode } from 'vue'
+import type { VNode } from 'vue';
 
 export interface Button {
-  text: string
-  url: string
+  text: string;
+  url: string;
 }
 
 export interface HeroBlock {
-  title: string
-  button: Button
-  subtitle?: string
-  className?: string
-  image?: string
+  title: string;
+  button: Button;
+  subtitle?: string;
+  className?: string;
+  image?: Image;
 }
 
 export interface Image {
-  src: string
-  alt: string
-  className?: string
+  src: string;
+  alt: string;
+  className?: string;
 }
 
-type InfoBlockDirection = 'horizontal' | 'vertical'
+type InfoBlockDirection = 'horizontal' | 'vertical';
 
 export interface InfoBlock {
-  title: string
-  text: string
-  iconUrl: string
-  direction?: InfoBlockDirection
-  className?: string
-  image?: Image
-  secondColumnContent?: VNode
+  title: string;
+  text: string;
+  iconUrl: string;
+  direction?: InfoBlockDirection;
+  className?: string;
+  image?: Image;
+  secondColumnContent?: VNode;
 }
 
-export interface Card {
-  title: string
-  text: string
-  image: Image
-  onClick: () => void
-  className?: string
+export interface CardBlock {
+  title: string;
+  text: string;
+  image: Image;
+  onClick: unknown;
+  className?: string;
 }
