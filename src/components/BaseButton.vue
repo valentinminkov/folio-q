981a3aa7b7f55e8e6b82706f38e9cea23192d64f
valentinminkov <minkowalentin@proton.me>
2023-11-26 21:04:24 +0100

    button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+export interface Button {
+  text: string;
+  url?: string;
+  onClick?: () => void;
+  className?: string;
+  icon?: unknown;
+}
+
+const { text, className, onClick, url } = defineProps<Button>();
+</script>
+
+<template>
+  <q-btn
+    class="button"
+    color="accent"
+    text-color="white"
+    size="xl"
+    :label="text"
+    :class="className"
+    :on-click="onClick"
+    :href="url"
+    :v-text="text"
+  />
+</template>
+
+<style scoped lang="scss">
+.button {
+  border-radius: 0;
+}
+</style>
