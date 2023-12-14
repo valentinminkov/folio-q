747cfc041e6e7f2e2280b1d15a0e4bc84ec0b256
valentinminkov <minkowalentin@proton.me>
2023-12-15 00:40:45 +0100

    create text block component

---
+++ src/components/TextBlock.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { type Button } from 'components/types/components';
+import BaseButton from 'components/BaseButton.vue';
+
+interface TextBlock {
+  title?: string;
+  subtitle?: string;
+  description?: string;
+  button?: Button;
+}
+
+const { title, subtitle, description, button } = defineProps<TextBlock>();
+</script>
+
+<template>
+  <div class="container" v-if="title || subtitle || description">
+    <h1 v-if="title">{{ title }}</h1>
+    <h4 v-if="subtitle">
+      {{ subtitle }}
+    </h4>
+    <p v-if="description">
+      {{ description }}
+    </p>
+    <div v-if="button && button.text && button.url">
+      <BaseButton :text="button.text" :url="button.url" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '../css/vars';
+@import '../css/mixins';
+.container {
+  display: flex;
+  flex-direction: column;
+  h1 {
+  }
+  h4 {
+  }
+  p {
+  }
+}
+</style>
